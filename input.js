b2984d18578b6f872c9b8d7ef6426487a62fd0d4
Nader Nasr <nadernasr7@gmail.com>
2021-12-01 17:08:57 -0500

    add control commands

--- input.js
+++ input.js
@@ -17,7 +17,18 @@ const handleUserInput = (data) => {
     if (key === '\u0003') { //Exit application
       process.exit();
     }
-    
+    if (key === 'w') {
+      connection.write("Move: up");
+    }
+    if (key === "s") {
+      connection.write("Move: down");
+    }
+    if (key === "a") {
+      connection.write("Move: left");
+    }
+    if (key === "d") {
+      connection.write("Move: right");
+    }
   });
   
 };
